7255e664296d0d35f8ee60998b539fd004844a88
Josh <gross.joshua.b@gmail.com>
2026-08-26 14:55:33 -0700

    afternoon review code

---
+++ src/Mon0824.java
@@ -0,0 +1,36 @@
+public class Mon0824 {
+    public static void main(String[] args) {
+        System.out.println(5);
+
+        System.out.println("Hello, world");
+
+        int x = 7;
+        System.out.println(x);
+        x = 12;
+        System.out.println(x);
+        x = 14;
+        int y = -47;
+        x = y;
+        System.out.println(x);
+        System.out.println(y);
+
+        x = 300;
+        System.out.println(x);
+        System.out.println(y);
+
+        int z = x + 5;
+
+//        x + 1;
+        x = x + 1;
+
+        int a = 5;
+        int b = a - 3;
+        int c = b * 5;
+        int d = c / 2;
+        System.out.println(a + " " + b + " " + c + " " + d);
+        int e = c % 3; // remainder operator (mod modulus)
+        System.out.println(e);
+
+
+    }
+}
